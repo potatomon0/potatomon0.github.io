0a6f54cad7a516cb3f9ba2232bcf1abd12dcf37a
Hu <huang.h0709@gmail.com>
2023-08-03 10:50:24 -0400

    main text is separated into 4 lines, each will have it's own animation delay. working on timing and speed of animation.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -19,21 +19,23 @@ function Main() {
                 {/* <div className="wrapper"> */}
                 {/* <div className="mainIntro"> */}
                 <div className="textContainer">
-                <h4 className="mainTextP1">Hello, my name is Hu and I am a current learner at Per Scholas accelerated</h4>
-                <h4 className="mainTextP2">
-                Software Engineering program where I am trained on Frontend and Backend Technologies. 
-                </h4>
+                <div className="mainTextP1">Hello, my name is Hu and I am a current learner at Per Scholas accelerated</div>
+                <div className="mainTextP2">
+                Software Engineering program where I am trained on Frontend and Backend 
+                </div>
+                <div className="mainTextP3">
+                Technologies. I have utilized ReactJS to create projects including this webpage. 
+                </div>
+                <div className="mainTextP4">
+                I also have made projects with only HTML5, CSS, and Javascript.
+                </div>
                 </div>
                 {/* </div>}
 
                 {/* </div>                 */}
                 {/* 
-                <h4 className="mainTextP3">
-                I have utilized ReactJS to create projects including this webpage. 
-                </h4>
-                <h4 className="mainTextP4">
-                I also have made projects with only HTML5, CSS, and Javascript.
-                </h4> */}
+                
+                 */}
                 <div className="rightArrowBox">
                     <div>
                     <Dropdown />
