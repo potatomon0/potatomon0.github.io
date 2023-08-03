e4e8744baf4338548204e8caef0459957a32bf7c
Hu <huang.h0709@gmail.com>
2023-08-03 13:22:28 -0400

    added open resume as pdf feature. styling pdf button/icon

--- src/components/MyLinks.jsx
+++ src/components/MyLinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import resume from '../files/hhResume.pdf'
 
 function MyLinks() {
     return (
@@ -10,6 +11,9 @@ function MyLinks() {
             <div className='github'>
                 <a href="https://github.com/potatomon0" target='_blank' rel="noopener noreferrer">
                 <img src="https://www.shareicon.net/data/2015/09/15/101512_logo_512x512.png" alt="" className='githubIcon'/></a>
+            </div>
+            <div className="resume">
+                <a href={resume} target="_blank"><img src="https://p1.hiclipart.com/preview/562/309/209/interview-icon-resume-icon-portfolio-icon-technology-line-computer-icons-meter-line-art-png-clipart.jpg" alt="" className="githubIcon"/></a>
             </div>
         </div>
     )
