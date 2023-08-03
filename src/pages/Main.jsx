547273c85203110e7127271299c2f97dd35663f3
Hu <huang.h0709@gmail.com>
2023-08-03 11:06:20 -0400

    changed dropdown item text color

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -19,33 +19,27 @@ function Main() {
                 {/* <div className="wrapper"> */}
                 {/* <div className="mainIntro"> */}
                 <div className="textContainer">
-                <div className="mainTextP1">Hello, my name is Hu and I am a current learner at Per Scholas accelerated</div>
-                <div className="mainTextP2">
-                Software Engineering program where I am trained on Frontend and Backend 
+                    <div className="mainTextP1">Hello, my name is Hu and I am a current learner at Per Scholas accelerated</div>
+                    <div className="mainTextP2">
+                        Software Engineering program where I am trained on Frontend and Backend
+                    </div>
+                    <div className="mainTextP3">
+                        Technologies. I have utilized ReactJS to create projects including this webpage.
+                    </div>
+                    <div className="mainTextP4">
+                        I also have made projects with only HTML5, CSS, and Javascript.
+                    </div>
                 </div>
-                <div className="mainTextP3">
-                Technologies. I have utilized ReactJS to create projects including this webpage. 
-                </div>
-                <div className="mainTextP4">
-                I also have made projects with only HTML5, CSS, and Javascript.
-                </div>
-                </div>
-                {/* </div>}
-
-                {/* </div>                 */}
-                {/* 
-                
-                 */}
                 <div className="rightArrowBox">
                     <div>
-                    <Dropdown />
+                        <Dropdown />
                     </div>
                     <div>
-                    <Link to="/Projects" style={{ textDecoration: 'none' }}>
-                        <div className="rightArrow">{arrows.right}</div>
-                    </Link></div>
+                        <Link to="/Projects" style={{ textDecoration: 'none',color:'#C3CEDA'}}>
+                            <div className="rightArrow">{arrows.right}</div>
+                        </Link></div>
                 </div>
-                
+
             </div>
             <div className="mainWeather">
                 <Weather />
