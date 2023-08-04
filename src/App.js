ca664c2adaefca73585752fa71b834d54fdd5e91
Hu <huang.h0709@gmail.com>
2023-08-03 21:29:48 -0400

    added filler images and model file for projects

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom'
 import Weather from './components/Weather'
 import Main from './pages/Main'
 import Projects from './pages/Projects'
+import ProjectInfo from './pages/ProjectInfo'
 
 function App() {
   const { data, getIP } = useContext(AppContext)
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Projects" element={<Projects />} />
+        <Route path="/ProjectInfo" element={<ProjectInfo/>}/>
       </Routes>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,54 +1,63 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import './Projects.css'
 import MyLinks from '../components/MyLinks'
 import Dropdown from '../components/Dropdown'
 import arrows from '../models/arrows'
 import { Link } from 'react-router-dom'
 import ReactLoading from 'react-loading'
+import pkmImg from '../img/pokemonBudgetGame.png'
+import projectInfo from '../models/projectInfo'
 
 function Projects() {
   const [done, setDone] = useState(undefined);
   useEffect(() => {
-      setTimeout(() => {
-          setDone(true);
-      }, 600);
+    setTimeout(() => {
+      setDone(true);
+    }, 600);
   }, []);
   return (
     <>
-           {!done ? (
-                <div className='loading'><ReactLoading
-                    type={"bubbles"}
-                    color={"#C3CEDA"}
-                    height={100}
-                    width={100}
-                /></div>
-            ) : 
-    <div className="projectsMain">
-      <div className="left">
-        <div className="leftArrow">
-          <div>
-            <Dropdown />
+      {!done ? (
+        <div className='loading'><ReactLoading
+          type={"bubbles"}
+          color={"#C3CEDA"}
+          height={100}
+          width={100}
+        /></div>
+      ) :
+        <div className="projectsMain">
+          <div className="left">
+            <div className="leftArrow">
+              <div>
+                <Dropdown />
+              </div>
+              <Link to="/" style={{ textDecoration: 'none', color: '#C3CEDA' }}>
+                <div className="rightArrow">{arrows.left}</div>
+              </Link></div>
+            <div className="hideMe"></div>
           </div>
-          <Link to="/" style={{ textDecoration: 'none', color: '#C3CEDA' }}>
-            <div className="rightArrow">{arrows.left}</div>
-          </Link></div>
-          <div className="hideMe"></div>
+          <div className="projectContainer">
+            <h1 className="pageTitle">
+              Web Developer Portfolio
+            </h1>
+            <div className="projects">
+              {projectInfo.map((project) => {
+                <Link to="/ProjectInfo" pjName={project.name}>
+                </Link>
+              })}
+              // <Link to="/ProjectInfo">
+              //   <img src={pkmImg} alt="" className="pkmProjectImg" />
+              //   </Link>
+            </div>
           </div>
-        <div className="projectContainer">
-        <h1 className="pageTitle">
-        Web Developer Portfolio
-      </h1>
-      <div className="projects">
-      Projects</div>
-      </div>
-      <div  className="right">
-        <div className="mainMylinks">
-          <MyLinks />
+          <div className="right">
+            <div className="mainMylinks">
+              <MyLinks />
+            </div>
+            <div className="hideMe"></div>
+          </div>
         </div>
-        <div className="hideMe"></div>
-        </div>
-    </div>
-}
+      }
     </>
   )
 }
