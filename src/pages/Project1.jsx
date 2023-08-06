3bff3a460dd227d3aa7335d54afe7a08a12e2bb5
Hu <huang.h0709@gmail.com>
2023-08-05 21:38:47 -0400

    made css box wireframe for project page

--- src/pages/Project1.jsx
+++ src/pages/Project1.jsx
@@ -4,13 +4,16 @@ import './Project.css'
 function Project1() {
   return (
     <div className="projectMain">
-        <div className="title">Project Title</div>
+        <div className="leftBtn"> left </div>
+        <div className="center">
+        <h2 className="title">Project Title</h2>
         <div className="info">Project information...</div>
         <div className="pjLink">Link button to prject</div>
         <div className="pjImgCarousel">PJ IMG Carousel</div>
         <div className="about">About this project</div>
         <div className="technical"></div>
-        
+    </div>
+    <div className="rightBtn"> right </div>
     </div>
   )
 }
